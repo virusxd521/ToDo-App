9c99aa5c5627671779ea895072d9153ec1bb955a
Daniel <virusxd521@gmail.com>
2021-07-22 16:15:10 +0300

    Adding new features :)

--- src/index.js
+++ src/index.js
@@ -3,25 +3,44 @@ const header_div = document.getElementsByClassName("div")[0];
 const header = document.querySelector("header");
 header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
 const body = document.querySelector("body");
+const form = document.querySelector("#form");
+
 
 header_div.addEventListener("click", event => {
     if (event.target.tagName === "IMG"){
-        if(event.target.attributes.src.value === "./styles/images/icon-moon.svg"){
+        
+        function switchToDarkTheme(){
             event.target.attributes.src.value = "./styles/images/icon-sun.svg";
             header.style.backgroundImage = "url(./styles/images/bg-mobile-dark.jpg)";
             body.style.backgroundColor = "rgb(50, 42, 78)";
-        } else if(event.target.attributes.src.value === "./styles/images/icon-sun.svg"){
-            body.style.backgroundColor = "white";
+        }
+
+        function switchToDefaultTheme(){
+            body.style.backgroundColor = "rgb(246, 246, 246)";
             header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
             event.target.attributes.src.value = "./styles/images/icon-moon.svg";
-            
-            
         }
-        console.log(event.target);
-        console.log(event.target.attributes.src.value);
+
+        if(event.target.attributes.src.value === "./styles/images/icon-moon.svg"){
+            switchToDarkTheme();
+        } else if(event.target.attributes.src.value === "./styles/images/icon-sun.svg"){
+            switchToDefaultTheme();
+        }
+
     }
     
 })
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+  
+});
+
+
+
+
+
 
 
 
