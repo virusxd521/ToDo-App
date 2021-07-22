af32858ba9f29ae48c92f4e16f6fb0f8031fc5b8
Daniel <virusxd521@gmail.com>
2021-07-22 09:58:40 +0300

    Added headers button functionallity

--- src/bundle.js
+++ src/bundle.js
@@ -6926,11 +6926,35 @@ exports.preventOverflow = preventOverflow$1;
 
 
 },{"@popperjs/core":1}],3:[function(require,module,exports){
-const bootstrap = require('../node_modules/bootstrap/dist/js/bootstrap');
+const bootstrap = require('bootstrap');
+const header_div = document.getElementsByClassName("div")[0];
+const header = document.querySelector("header");
+header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
+const body = document.querySelector("body");
+
+header_div.addEventListener("click", event => {
+    if (event.target.tagName === "IMG"){
+        if(event.target.attributes.src.value === "./styles/images/icon-moon.svg"){
+            event.target.attributes.src.value = "./styles/images/icon-sun.svg";
+            header.style.backgroundImage = "url(./styles/images/bg-mobile-dark.jpg)";
+            body.style.backgroundColor = "rgb(50, 42, 78)";
+        } else if(event.target.attributes.src.value === "./styles/images/icon-sun.svg"){
+            body.style.backgroundColor = "white";
+            header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
+            event.target.attributes.src.value = "./styles/images/icon-moon.svg";
+            
+            
+        }
+        console.log(event.target);
+        console.log(event.target.attributes.src.value);
+    }
+    
+})
 
 
-console.log(bootstrap);
-},{"../node_modules/bootstrap/dist/js/bootstrap":2}],4:[function(require,module,exports){
+
+
+},{"bootstrap":2}],4:[function(require,module,exports){
 // shim for using process in browser
 var process = module.exports = {};
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const bootstrap = require('bootstrap');
+const header_div = document.getElementsByClassName("div")[0];
+const header = document.querySelector("header");
+header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
+const body = document.querySelector("body");
+
+header_div.addEventListener("click", event => {
+    if (event.target.tagName === "IMG"){
+        if(event.target.attributes.src.value === "./styles/images/icon-moon.svg"){
+            event.target.attributes.src.value = "./styles/images/icon-sun.svg";
+            header.style.backgroundImage = "url(./styles/images/bg-mobile-dark.jpg)";
+            body.style.backgroundColor = "rgb(50, 42, 78)";
+        } else if(event.target.attributes.src.value === "./styles/images/icon-sun.svg"){
+            body.style.backgroundColor = "white";
+            header.style.backgroundImage = "url(./styles/images/bg-mobile-light.jpg)";
+            event.target.attributes.src.value = "./styles/images/icon-moon.svg";
+            
+            
+        }
+        console.log(event.target);
+        console.log(event.target.attributes.src.value);
+    }
+    
+})
 
 
-console.log(bootstrap);
+
